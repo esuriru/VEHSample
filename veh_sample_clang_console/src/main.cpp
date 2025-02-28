0d5420d9ace92eb14e6f0c1fc223eba3a6efa2c8
Christian <97773844+esuriru@users.noreply.github.com>
2025-02-28 17:16:55 +0800

    Add creation of `crash_log.txt` and printing of stacktrace

--- veh_sample_clang_console/src/main.cpp
+++ veh_sample_clang_console/src/main.cpp
@@ -1,14 +1,75 @@
+// For `fopen`
+#define _CRT_SECURE_NO_WARNINGS
+
 #include <windows.h>
-#include <iostream>
+#include <dbghelp.h>
+#include <stdio.h>
+
+// Declare memory to be used for the stacktrace buffer, so when
+// the error is not having enough memory left, we will stil be able to
+// print out the stack trace.
+namespace
+{
+    void* stack_trace_buffer[100];
+}
 
 LONG CALLBACK exception_handler(PEXCEPTION_POINTERS exception_info) 
 {
     if (exception_info->ExceptionRecord->ExceptionCode == 
         EXCEPTION_ACCESS_VIOLATION) 
     {
-        std::cout << "Access violation caught by VEH" << std::endl;
+        printf("%s", "Access violation caught by VEH\n");
 
-        system(CRASH_HANDLER_FILENAME); 
+        // Capture stack trace
+        USHORT frames = CaptureStackBackTrace(0, 100, stack_trace_buffer, NULL);
+
+        // Initialize symbol handling
+        SymInitialize(GetCurrentProcess(), NULL, TRUE);
+
+        // Print stack trace
+        FILE* file = fopen(CRASH_LOG_FILENAME, "w");
+        if (!file)
+        {
+            printf("Could not create %s\n", CRASH_LOG_FILENAME);
+        }
+
+        fputs("Stacktrace\n", file);
+
+        for (USHORT i = 0; i < frames; ++i) 
+        {
+            DWORD64 address = (DWORD64)stack_trace_buffer[i];
+            SYMBOL_INFO symbol = { sizeof(SYMBOL_INFO), 0, 0, 0, 0, 0 };
+
+            if (SymFromAddr(GetCurrentProcess(), address, 0, &symbol)) 
+            {
+                fprintf(file, "%s - 0x%p\n", symbol.Name, (void*)address);
+
+                IMAGEHLP_LINE64 line = { sizeof(IMAGEHLP_LINE64) };
+                DWORD displacement;
+                if (SymGetLineFromAddr64(
+                    GetCurrentProcess(), 
+                    address, 
+                    &displacement, 
+                    &line))
+                {
+                    fprintf(file, "%s:%lu\n", line.FileName, line.LineNumber);
+                }
+
+            }
+            else
+            {
+                fprintf(file, "0x%p (Symbol information not available)\n",
+                    (void*)address);
+            }
+        }
+
+        fclose(file);
+
+        SymCleanup(GetCurrentProcess());
+
+        const char* crash_handler_filename = CRASH_HANDLER_FILENAME;
+
+        system(crash_handler_filename); 
         TerminateProcess(GetCurrentProcess(), 1);
     }
 
@@ -17,7 +78,7 @@ LONG CALLBACK exception_handler(PEXCEPTION_POINTERS exception_info)
 
 int main()
 {
-    std::cout << "Causing access violation" << std::endl;
+    printf("%s", "Causing access violation\n");
 
     AddVectoredExceptionHandler(1, exception_handler);
 
--- veh_sample_placeholder_crash_handler/src/main.cpp
+++ veh_sample_placeholder_crash_handler/src/main.cpp
@@ -1,6 +1,24 @@
-#include <iostream>
+#include <iostream> 
+#include <fstream>
+#include <string>
 
 int main()
 {
     std::cout << "The program has crashed!" << std::endl;
+
+    std::ifstream crash_log_file(CRASH_LOG_FILENAME);
+    if (!crash_log_file.is_open())
+    {
+        std::cout << "Could not open crash log" << std::endl;
+    }
+
+    std::string line;
+    while (std::getline(crash_log_file, line))
+    {
+        std::cout << line << std::endl;
+    }
+
+    crash_log_file.close();
+
+    return 0;
 }
